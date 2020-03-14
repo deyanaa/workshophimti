0d5e891439aebfc03b9e3317203763c9dfe3fb35
deyanaa <deyanamaurer89@gmail.com>
2020-03-14 11:51:27 +0700

    merubah tampilan welcome

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    workshop
+                    deyanaa
                 </div>
 
                 <div class="links">
